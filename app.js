29420d5de0e132818a0477ff6c700f87a4081194
Sathira-Wijeratne <swije27@gmail.com>
2023-03-04 11:03:41 +0530

    Async/Await

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const fs = require('fs');
-fs.readFile('file.txt', 'utf8', function (err, data) {
- if (err) throw err;
- console.log(data);
-});
+async function myFunction() {
+    try {
+    const result = await myPromise;
+    console.log(result);
+    } catch (error) {
+    console.log(error);
+    }
+   }
+   myFunction();
